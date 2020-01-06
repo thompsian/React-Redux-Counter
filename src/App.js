55b5ea0125d82cf533a92ac3156608c0ef3b457d
thompsian <thompsian@gmail.com>
2020-01-05 19:06:44 -0800

    Counter implemented with Connect

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from "react"
 import {connect} from "react-redux"
+import {increment, decrement} from "./redux"
 
 function App(props) {    
     return (
         <div>
             <h1>{props.count}</h1>
-            <button>-</button>
-            <button>+</button>
+            <button onClick={props.decrement}>-</button>
+            <button onClick={props.increment}>+</button>
         </div>
     )
 }
 
-function mapStateToProps(globalState) {
-  // return an object where the keys are the name of the prop your component wants,
-  // values are the actual parts of the global state your component wants
-  return {
-      count: globalState
-  }
+function mapStateToProps(state) {
+    return {
+        count: state
+    }
 }
 
-//this can also be formatted like
-// const mapStateToProps = (globalState) => ({count: globalState})
+const mapDispatchToProps = {
+  increment: increment,
+  decrement: decrement
+}
 
-export default connect(mapStateToProps, {})(App)
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import store from "./redux"
 import {Provider} from "react-redux"
+import store from "./redux"
 
 import App from "./App"
 
