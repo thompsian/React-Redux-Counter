946e5a1a55841a4270ddd0a767e030bd77320559
thompsian <thompsian@gmail.com>
2020-01-05 19:17:23 -0800

    refactor with hooks

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from "react"
-import {connect} from "react-redux"
+import {useSelector, useDispatch} from "react-redux"
 import {increment, decrement} from "./redux"
 
-function App(props) {    
+function App(props) {
+    const count = useSelector(state => state)
+    const dispatch = useDispatch()
     return (
         <div>
-            <h1>{props.count}</h1>
-            <button onClick={props.decrement}>-</button>
-            <button onClick={props.increment}>+</button>
+            <h1>{count}</h1>
+            <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(increment())}>+</button>
         </div>
     )
 }
 
-function mapStateToProps(state) {
-    return {
-        count: state
-    }
-}
-
-const mapDispatchToProps = {
-  increment: increment,
-  decrement: decrement
-}
-
-
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default App
